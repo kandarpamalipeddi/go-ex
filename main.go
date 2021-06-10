21411c5a38675065fad7bbf9e6db7c86133dc87c
Kandarpa Malipeddi <69784217+kandarpamalipeddi@users.noreply.github.com>
2021-06-10 11:57:44 +0530

    Update main.go
    Fixed errors

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
+package main
 import (
         "fmt"
         "net/http"
         )
 
-http.Handle("/", http.FileServer("./"))
-err := http.ListenAndServe("3000", nil)
-if(err != nil) {
-        fmt.Println("Failed to start file server on port : ", port, err)
+func main() {
+        http.Handle("/", http.FileServer("./"))
+        err := http.ListenAndServe(":3000", nil)
+        if(err != nil) {
+                fmt.Println("Failed to start file server on port 3000, ", port, err)
+        }
 }
