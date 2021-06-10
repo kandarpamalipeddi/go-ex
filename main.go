1e36a030341af7fe7aeecf0ec3c7045a3adf9836
Kandarpa Malipeddi <69784217+kandarpamalipeddi@users.noreply.github.com>
2021-06-10 13:38:07 +0530

    Fixed compilation error with dirctory

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
         )
 
 func main() {
-        http.Handle("/", http.FileServer("./"))
+        http.Handle("/", http.FileServer(http.Dir("./")))
         err := http.ListenAndServe(":3000", nil)
         if(err != nil) {
                 fmt.Println("Failed to start file server on port 3000, ", port, err)
