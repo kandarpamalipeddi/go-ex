eabe7582d56eae69dc2a94a735ec06935144fd9e
Kandarpa Malipeddi <69784217+kandarpamalipeddi@users.noreply.github.com>
2021-06-10 11:50:28 +0530

    Update main.go
    Using static 3000 port

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
         )
 
 http.Handle("/", http.FileServer("./"))
-err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+err := http.ListenAndServe(fmt.Sprintf(":%s", 3000), nil)
 if(err != nil) {
         fmt.Println("Failed to start file server on port : ", port, err)
 }
