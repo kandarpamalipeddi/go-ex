99b312048d18ec12f0f8af59f557badd240fb8a1
Kandarpa Malipeddi <69784217+kandarpamalipeddi@users.noreply.github.com>
2021-10-26 14:12:18 +0530

    Update main.go

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
         )
 
 func main() {
+        
+        // Comment lines to check the PR template option
         http.Handle("/", http.FileServer(http.Dir("./")))
         err := http.ListenAndServe(":3000", nil)
         if(err != nil) {
